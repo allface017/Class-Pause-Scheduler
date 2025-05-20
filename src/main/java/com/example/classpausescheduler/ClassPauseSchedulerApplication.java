aeffb0f53b6222669bfe56d4b7de7eecf2cfd94b
allface017 <s.muramori.sys22@morijyobi.ac.jp>
2025-05-20 14:53:17 +0900

    first commit

---
+++ src/main/java/com/example/classpausescheduler/ClassPauseSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.classpausescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassPauseSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassPauseSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/classpausescheduler/ClassPauseSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classpausescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassPauseSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

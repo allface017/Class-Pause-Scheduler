1418fae5d51a6b539d1b69eda8564aadeddd0fed
allface017 <s.muramori.sys22@morijyobi.ac.jp>
2025-06-02 14:16:46 +0900

    休講情報を登録するマッパーを定義

--- src/main/java/com/example/classpausescheduler/mapper/ClassPauseMapper.java
+++ src/main/java/com/example/classpausescheduler/mapper/ClassPauseMapper.java
@@ -3,6 +3,8 @@ package com.example.classpausescheduler.mapper;
 import com.example.classpausescheduler.entity.ClassPause;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
 
 import java.util.List;
 
@@ -10,4 +12,9 @@ import java.util.List;
 public interface ClassPauseMapper {
     @Select("SELECT * FROM class_pause")
     List<ClassPause> selectAllclasspause();
+
+    // 休講情報の登録
+    @Insert("INSERT INTO class_pause (subject_id, date, period, type, substitute_date, substitute_period, remarks) VALUES (#{subjectId}, #{date}, #{period}, #{type}, #{substitute_date}, #{substitute_period}, #{remarks})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insertClassPause(ClassPause classPause);
 }

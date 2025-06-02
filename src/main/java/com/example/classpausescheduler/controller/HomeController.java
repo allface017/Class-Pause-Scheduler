bc20c61826b71ff70d86e4ff2febcd6e98fa0f84
allface017 <s.muramori.sys22@morijyobi.ac.jp>
2025-06-02 14:02:14 +0900

    講義情報・休講情報を取得するコントロールを定義

--- src/main/java/com/example/classpausescheduler/controller/HomeController.java
+++ src/main/java/com/example/classpausescheduler/controller/HomeController.java
@@ -1,15 +1,32 @@
 package com.example.classpausescheduler.controller;
 
+import com.example.classpausescheduler.entity.ClassPause;
+import com.example.classpausescheduler.entity.Subject;
+import com.example.classpausescheduler.service.ClassPauseService;
+import com.example.classpausescheduler.service.SubjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
+    private final ClassPauseService classPauseService;
+    private final SubjectService subjectService;
+
+    public HomeController(ClassPauseService classPauseService, SubjectService subjectService) {
+        this.classPauseService = classPauseService;
+        this.subjectService = subjectService;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("title", "Class Pause Scheduler");
+        List<Subject> subjects = subjectService.getAllSubject();
+        List<ClassPause> class_pauses = classPauseService.getAllClassPause();
+        model.addAttribute("subjects", subjects);
+        model.addAttribute("class_pauses", class_pauses);
         return "Home/index";
     }
 }
